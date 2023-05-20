a892cdbe57309f56fb9ae8d402c34585e5499300
yo1 <110065476+yo1am1@users.noreply.github.com>
2023-05-20 15:00:21 +0300

    Update

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import PhoneForm
+from .models import Number
+from .tasks import sending
+
+
+def add_number(request):
+    if request.method == "POST":
+        form = PhoneForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            num_clean = form.cleaned_data["number"]
+            sending.delay(num_clean)
+
+            return HttpResponseRedirect(reverse("send_message"))
+    else:
+        form = PhoneForm()
+    return render(request, "main.html", {"form": form})
+
+
+def message(request):
+    return render(request, "answer.html")
